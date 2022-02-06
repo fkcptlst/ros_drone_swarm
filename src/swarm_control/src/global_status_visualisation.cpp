e4b866e06522429122668ecaf3cafe83354b7fef
fkcptlst <kevinfinley@163.com>
2022-02-06 15:17:17 +0800

    global visualisation restructured

--- src/swarm_control/src/global_status_visualisation.cpp
+++ src/swarm_control/src/global_status_visualisation.cpp
@@ -2,7 +2,7 @@
  * @Author: lcf
  * @Date: 2022-01-31 21:34:24
  * @LastEditors: lcf
- * @LastEditTime: 2022-02-06 14:55:27
+ * @LastEditTime: 2022-02-06 15:14:20
  * @FilePath: /swarm_ws2/src/swarm_control/src/global_status_visualisation.cpp
  * @Description: This node observes position of all vehicles and unicasts relevant info to each vehicle, modified from swarm_controller
  * 
@@ -12,23 +12,29 @@
 #include "uav_planner.h"
 #include "visualisation_utils.h"
 
+#include <nav_msgs/Odometry.h>
+
 using namespace std;
 
 
 ros::Subscriber Drone_state_sub[MAX_UAV_NUM+1]; //用于订阅drone_state
 ros::Subscriber Commitment_sub[MAX_UAV_NUM+1]; //用于订阅commitment
 
+ros::Subscriber Gazebo_odom_sub[MAX_UAV_NUM+1]; //用于订Odometry
+
+
 prometheus_msgs::DroneState DroneStateList[MAX_UAV_NUM+1];
 prometheus_msgs::Commitment CommitmentList[MAX_UAV_NUM+1];
+nav_msgs::Odometry GazeboOdomList[MAX_UAV_NUM+1];
+
 
 bool DroneStateValidFlgList[MAX_UAV_NUM+1]; //用于标记是否有效，避免发布已经失效的无人机信息
 
 Eigen::Vector3d dronePos[MAX_UAV_NUM+1]; //drone position vector
-uint32_t drone_msgseq[MAX_UAV_NUM+1]; //msg counter, 防止使用初始的0位置导致无人机死锁
-
 
 void drone_state_topicUpdate_cb(const prometheus_msgs::DroneState::ConstPtr& state_msg, int drone_id);
 void commitment_topicUpdate_cb(const prometheus_msgs::Commitment::ConstPtr& commitment_msg, int drone_id);
+void odomUpdate_cb(const nav_msgs::Odometry::ConstPtr& odom_msg, int drone_id); //更新每个无人机pos信息的callback function
 
 void globalUpdate_cb(const ros::TimerEvent &e);
 void initialize();
@@ -58,6 +64,7 @@ int main(int argc, char **argv)
     for(int i = 1; i <= swarm_num_uav; i++) 
     {
         Drone_state_sub[i] = nh.subscribe<prometheus_msgs::DroneState>("/uav"+std::to_string(i)+ "/prometheus/drone_state", 3, boost::bind(drone_state_topicUpdate_cb,_1,i));//TODO
+        Gazebo_odom_sub[i] = nh.subscribe<nav_msgs::Odometry>("/uav"+std::to_string(i)+ "/prometheus/ground_truth", 3, boost::bind(odomUpdate_cb,_1,i));//TODO
         Commitment_sub[i] = nh.subscribe<prometheus_msgs::Commitment>("/uav"+std::to_string(i)+ "/prometheus/commitment", 10, boost::bind(commitment_topicUpdate_cb,_1,i));
     }
 
@@ -76,15 +83,18 @@ void initialize()
 {
     for(int i = 0; i <= MAX_UAV_NUM; i++) 
     {
-        drone_msgseq[i] = 0;
         dronePos[i] = Eigen::Vector3d::Zero();
     }
+}
+void odomUpdate_cb(const nav_msgs::Odometry::ConstPtr& odom_msg, int drone_id) //更新每个无人机pos信息的callback function
+{
+    GazeboOdomList[drone_id] = *odom_msg;
+    dronePos[drone_id] = Eigen::Vector3d(odom_msg->pose.pose.position.x,odom_msg->pose.pose.position.y,odom_msg->pose.pose.position.z);
 }
 void drone_state_topicUpdate_cb(const prometheus_msgs::DroneState::ConstPtr& state_msg, int drone_id) //更新每个无人机信息的callback function
 {
     DroneStateList[drone_id] = *state_msg;
-    drone_msgseq[drone_id] = state_msg->header.seq;
-    dronePos[drone_id]  = Eigen::Vector3d(state_msg->position[0], state_msg->position[1], state_msg->position[2]); //get position
+    
     if(state_msg->armed == true) //TODO: need further check
         DroneStateValidFlgList[drone_id] = true; //标记获得新消息有效
     else
@@ -101,8 +111,6 @@ void globalUpdate_cb(const ros::TimerEvent &e) //publish更新所有无人机信
     for(int i = 1; i <= swarm_num_uav; i++)
     {
 
-        if(drone_msgseq[i] != 0)//check validity
-        {
                 if(DroneStateValidFlgList[i] == true)
                 {
                     generateCommitmentMarker(markerArray, CommitmentList[i],dronePos[i], i);
@@ -112,7 +120,6 @@ void globalUpdate_cb(const ros::TimerEvent &e) //publish更新所有无人机信
                 {
                     ROS_INFO("uav_id: %d is disarmed",i);
                 }
-        }
     }
     /*
     for(int i = 0; i <= MAX_UAV_NUM; i++)
